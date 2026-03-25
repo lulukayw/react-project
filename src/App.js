bab3632a375cb698c305d3bc00966fe9100125b6
Lulu Wilson - lulukayw <lulu@kay-wilson.net>
2026-03-24 22:23:18 -0400

    created reactive board

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-function Square({ value }) {
-  return <button className="square">{value}</button>;
+import { useState } from 'react';
+
+function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue('X');
+  }
+  return (
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
 }
 
 export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value="1"/>
-        <Square value="1"/>
-        <Square value="1"/>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="1"/>
-        <Square value="1"/>
-        <Square value="1"/>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="1"/>
-        <Square value="1"/>
-        <Square value="1"/>
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
